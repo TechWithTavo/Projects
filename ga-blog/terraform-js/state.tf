8ce4b3b376e2f47d0103b8e0b12e88542720112b
TechWithTavo <aguilargustavo968@gmail.com>
2025-04-23 19:42:17 -0400

    committing changes to the files

--- ga-blog/terraform-js/state.tf
+++ ga-blog/terraform-js/state.tf
@@ -22,32 +22,3 @@ resource "aws_cloudfront_distribution" "nextjs_distribution" {
       origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
     }
   }
-  enabled             = true
-  is_ipv6_enabled = true
-  comment = "Next.js portfolio site"
-  default_root_object = "index.html"
-}
-  
-  default_cache_behavior {
-    allowed_methods = ["GET", "HEAD", "OPTIONS"]
-    cached_methods  = ["GET", "HEAD"]   
-    target_origin_id = "S3-nextjs-portfolio-bucket-ga" 
-
-    forwarded_values {
-      query_string = false
-    }
-      cookies {
-        forward = "none"
-      }
-
-    viewer_protocol_policy = "redirect-to-https"
-    min_ttl = 0
-    default_ttl = 3600
-    max_ttl = 86400
-    }
-    viewer_certificate {
-      
-    }
-    restrictions {
-    
-    }
